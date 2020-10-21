7cadc6af21b6fb6cf0b383888a5ed66d3530f980
Jeremy Kato <jeremy.kato@crisphealth.org>
2020-10-21 10:02:54 -0400

    Initialize Service

---
+++ Controllers/SlowController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SlowServer.Controllers
+{
+    [ApiController]
+    public class SlowController : ControllerBase
+    {
+        private TimeSpan _sleepDuration;
+        public SlowController()
+        {
+            _sleepDuration = TimeSpan.FromSeconds(20);
+        }
+
+        [HttpGet("{*url}", Order = int.MaxValue)]
+        public IActionResult Get()
+        {
+            Thread.Sleep(_sleepDuration);
+            return Ok();
+        }
+
+        [HttpPost("{*url}", Order = int.MaxValue)]
+        public IActionResult Post()
+        {
+            Thread.Sleep(_sleepDuration);
+            return Ok();
+        }
+    }
+}
